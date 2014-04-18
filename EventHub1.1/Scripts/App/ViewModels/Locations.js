b59d86e21ff340e6e5447e67912023842497cd4f
Jose Alava <jalava85@gmail.com>
2014-04-17 22:02:56 -0400

    Spent some time trying to get the observable array to work; great failure.

---
+++ EventHub1.1/Scripts/App/ViewModels/Locations.js
@@ -0,0 +1,10 @@
+﻿define(["ViewModels/Location"], function () {
+
+    var locations = function (locationsDTO) {
+        var self = this;
+        self.locations = ko.observableArray(locationsDTO);
+
+    }
+
+    return locations;
+})
