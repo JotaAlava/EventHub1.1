7a853d5e03776809d9e4cc2befad5f4b7c20b754
Jose Alava <jalava85@gmail.com>
2014-05-10 15:11:40 -0400

    Finally got KO down, it is working with the services. This is right before changing the UI to Vaca's UI.

--- EventHub1.1/Scripts/App/ViewModels/Locations.js
+++ EventHub1.1/Scripts/App/ViewModels/Locations.js
@@ -1,12 +1,42 @@
-﻿define(["ViewModels/Location"], function (location) {
+﻿define(["ViewModels/Location"], function (Location) {
 
     var locations = function (locationsDTO) {
         var self = this;
 
         self.locations = ko.observableArray();
+        
+        self.hideDetails = function () {
+            $('ul[id^="sportDetails_"]').toggle()
+        }
+
+        // Probably should find a better way of doing this - Circular Reference error
+        self.doSomething = function (formData) {
+            var result = (function (formData) {
+                var result = {
+                    name: formData[0].value,
+                    address: formData[1].value,
+                    active: formData[2].value == "on" ? true : false,
+                    id: 0
+                }
+
+                return result;
+            })(formData);
+
+            $.ajax({
+                dataType: "json",
+                url: "http://localhost:29196/location/",
+                context: document.body,
+                type: 'POST',
+                data: result
+            }).success(function (result) {
+                var newLocaiton = new Location(result);
+
+                self.locations.push(newLocaiton);
+            });
+        }
 
         for (var i = 0; i < locationsDTO.length; i++) {
-            self.locations.push(new location(locationsDTO[i]));
+            self.locations.push(new Location(locationsDTO[i]));
         }
 
         return self;
